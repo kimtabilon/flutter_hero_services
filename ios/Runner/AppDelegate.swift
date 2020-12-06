6c66994227c3fad9317189e76a007d726c95eb1e
Kim Tabilon <kim.gtabilon@gmail.com>
2020-12-06 09:44:40 +0800

    added: config firebase for ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,6 +11,7 @@ import GoogleMaps
   ) -> Bool {
 
     GMSServices.provideAPIKey("AIzaSyCa74d9dW86yh-52LzCy9S0awYbgXuZ79w")
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
